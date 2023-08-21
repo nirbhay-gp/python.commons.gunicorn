58489d8ff5a46d3a862df16877f5a86532c36226
Nirbhay Gupta <nirbhay.gupta@junio.in>
2023-08-21 11:14:49 +0530

    project setup + initial code

--- .scripts/build-server-image.sh
+++ .scripts/build-server-image.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 pipenv requirements > requirements.txt
-docker build --platform linux/amd64 -f .docker/Dockerfile.Server -t junio/gunicorn:local .
+docker build --platform linux/amd64 -f .docker/Dockerfile.Server -t gunicorn:local .
--- .scripts/publish-server-image.sh
+++ .scripts/publish-server-image.sh
@@ -4,6 +4,6 @@
     && echo $DOCKER_REPO \
     && REMOTE_IMAGE="${DOCKER_REPO}/gunicorn:${SERVER_VERSION}" \
     && echo $REMOTE_IMAGE \
-    && docker tag junio/gunicorn:local $REMOTE_IMAGE\
+    && docker tag gunicorn:local $REMOTE_IMAGE\
     && docker push $REMOTE_IMAGE
 )
